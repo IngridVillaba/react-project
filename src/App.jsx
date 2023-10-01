ea8df3ec45de552f541eb846a3d6ce0eec710ab5
Ingrid Julieth <injuvillalba10@gmail.com>
2023-10-01 18:08:27 -0500

    content

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Badge from "./badge";
 import Card from "./card";
 import Header from "./header";
 import Footer from "./footer"
+import Counter from "./counter"
 
 import Rainbow from "./rainbow.jpeg"
 
@@ -17,7 +18,7 @@ function App (){
             {/* three cards */}
         <Card title ="Card 1" content = "Hello"/>
         <Card title ="Card 2" content = "Good day"/>
-        <Card title ="Card 3" content = "Happy"/>
+        <Card title ="Card 3" content = {<Counter/>}/>
 
         {/* Avatar */}
         <Avatar
